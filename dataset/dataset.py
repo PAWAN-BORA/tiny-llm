34cb1687a014a2bae5d098db03d66805e8ac3211
pawan <pawan.bora@appinventiv.com>
2026-08-12 13:47:28 +0530

    Add TextDataset File. Add uv project manager

---
+++ dataset/dataset.py
@@ -0,0 +1,32 @@
+import torch
+# from torch.utils.data import Dataset
+
+class TextDataset:
+    def __init__(self, tokens, context_length):
+        """
+        tokens:
+            Complete tokenized text.
+
+        context_length:
+            Number of tokens the model can see at once.
+        """
+        self.tokens = torch.tensor(tokens, dtype=torch.long)
+        self.context_length = context_length
+
+    def __len__(self):
+         # We need one additional token for the target.
+        return len(self.tokens) - self.context_length
+
+    def __getitem__(self, index):
+        # Input sequence
+        x = self.tokens[
+            index:index + self.context_length
+        ]
+
+        # Same sequence shifted by one token.
+        # This is what the model needs to predict.
+        y = self.tokens[
+            index + 1:index + self.context_length + 1
+        ]
+
+        return x, y
